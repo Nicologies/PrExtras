8178ef5020db80aa18e3f765109f086f3f4081fb
EbenZhang <zhangyingneng@gmail.com>
2016-04-30 12:34:36 +0800

    Extract the UserMapping to a Separate Plugin

--- prextras-common/src/main/java/com/nicologies/prextras/common/PrExtrasConstants.java
+++ prextras-common/src/main/java/com/nicologies/prextras/common/PrExtrasConstants.java
@@ -6,5 +6,5 @@ public class PrExtrasConstants {
     public final static String TokenAuthType = "Token";
     public final static String UsernameAuthType = "Username";
     public final static String SystemWideTokenAuthType = "SystemWideToken";
-    public final static String PrefixOfUserMapping = "prextras.user_mapping.";
+    public final static String PrefixOfUserMapping = "user_mapping.";
 }
